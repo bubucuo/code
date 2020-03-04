98532ce5c37345485a15f462caf8081694d79436
bubucuo <276185876@qq.com>
2020-03-04 11:20:08 +0800

    MyTaobaoPage拼写不一致 fixed

--- taobao/src/pages/MytaobaoPage/index.js
+++ taobao/src/pages/MytaobaoPage/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import PageLayout from "../../layout/PageLayout";
 
 export default class MyTaobaoPage extends Component {
-    render() {
-        return <PageLayout>MyTaobaoPage</PageLayout>;
-    }
+  render() {
+    return <PageLayout>MyTaobaoPage</PageLayout>;
+  }
 }
